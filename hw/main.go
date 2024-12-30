78c9e352a506cac74e700a3a46622db14ba16c7b
alXmas <alexsandr.xmas@gmail.com>
2024-12-30 12:07:42 +0400

    Add test

--- hw/main.go
+++ hw/main.go
@@ -1,11 +1,12 @@
 package main
 
 import "fmt"
+const englishHelloPrefix = "Hello, "
 
-func Hello() string {
-	return "Hello World!"
+func Hello(name string) string {
+  return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello())
+  fmt.Println(Hello("Xmas"))
 }
