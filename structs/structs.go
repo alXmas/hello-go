ae48565124626edd99b84c234224c700e8a3f077
alXmas <alexsandr.xmas@gmail.com>
2025-03-11 10:09:15 +0300

    update structure

--- structs/structs.go
+++ structs/structs.go
@@ -1,8 +1,33 @@
 package structs
 
 type Rectangle struct {
-	Height float64
 	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Shape interface {
+	Area() float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
 
 func Perimeter(rectangle Rectangle) float64 {
